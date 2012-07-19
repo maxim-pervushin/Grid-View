735da291d8347e42ed602ec9088351d905845b14
Maxim Pervushin <exxenos@gmail.com>
2012-07-19 15:21:55 +0400

    Initial Commit

---
+++ GridViewDemo/MFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MFAppDelegate.h
+//  GridViewDemo
+//
+//  Created by Maxim Pervushin on 7/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GridViewDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GridViewDemo
+//
+//  Created by Maxim Pervushin on 7/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MFAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MFAppDelegate class]));
+    }
+}
